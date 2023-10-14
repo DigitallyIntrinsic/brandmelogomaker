8e8bc1ab29bcbac21edaa3cc8081359c35446ef3
DigitallyIntrinsic <139934612+DigitallyIntrinsic@users.noreply.github.com>
2023-10-14 15:06:00 -0500

    finished shapechoice

--- index.js
+++ index.js
@@ -9,20 +9,26 @@ const { Triangle, Square, Circle } = require("./lib/shapes");
 
 // Function writes the SVG file using user answers from inquirer prompts
 function writeToFile(fileName, answers) {
-   
+
     let svgString = "";
 
-  svgString =
-  '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
-// <g> tag wraps <text> tag so that user font input layers on top of polygon -> not behind
-svgString += "<g>";
+    svgString =
+        '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
+    // <g> tag wraps <text> tag so that user font input layers on top of polygon -> not behind
+    svgString += "<g>";
 
-svgString += `${answers.shape}`;
+    svgString += `${answers.shape}`;
 
-  // Conditional check takes users input from choices array and then adds polygon properties and shape color to SVG string
-  let shapeChoice;
-  if (answers.shape === "Triangle") {
-    shapeChoice = new Triangle();
-    svgString += `<polygon points="150, 18 244, 182 56, 182" fill="${answers.shapeBackgroundColor}"/>`;
-  } else if (answers.shape === "Square") {
-    shapeChoice = new Square();
+    // Conditional check takes users input from choices array and then adds polygon properties and shape color to SVG string
+    let shapeChoice;
+    if (answers.shape === "Triangle") {
+        shapeChoice = new Triangle();
+        svgString += `<polygon points="150, 18 244, 182 56, 182" fill="${answers.shapeBackgroundColor}"/>`;
+    } else if (answers.shape === "Square") {
+        shapeChoice = new Square();
+        svgString += `<rect x="73" y="40" width="160" height="160" fill="${answers.shapeBackgroundColor}"/>`;
+    } else {
+        shapeChoice = new Circle();
+        svgString += `<circle cx="150" cy="115" r="80" fill="${answers.shapeBackgroundColor}"/>`;
+    }
+    

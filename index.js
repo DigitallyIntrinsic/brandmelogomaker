2ce8b68f528d7d2652680da3c0b01000be034e8b
DigitallyIntrinsic <139934612+DigitallyIntrinsic@users.noreply.github.com>
2023-10-14 15:03:59 -0500

    writetofile function

--- index.js
+++ index.js
@@ -11,3 +11,10 @@ const { Triangle, Square, Circle } = require("./lib/shapes");
 function writeToFile(fileName, answers) {
     // File starts as an empty string
     let svgString = "";
+      // Sets width and height of logo container
+  svgString =
+  '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
+// <g> tag wraps <text> tag so that user font input layers on top of polygon -> not behind
+svgString += "<g>";
+// Takes user input for shape choice and inserts it into SVG file
+svgString += `${answers.shape}`;

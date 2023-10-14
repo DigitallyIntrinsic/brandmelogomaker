b5ac8c6b4d95c455d3d5ab8bb2cb6435d1c7ac83
DigitallyIntrinsic <139934612+DigitallyIntrinsic@users.noreply.github.com>
2023-10-14 15:05:29 -0500

    edited file and added shapechoice

--- index.js
+++ index.js
@@ -9,12 +9,20 @@ const { Triangle, Square, Circle } = require("./lib/shapes");
 
 // Function writes the SVG file using user answers from inquirer prompts
 function writeToFile(fileName, answers) {
-    // File starts as an empty string
+   
     let svgString = "";
-      // Sets width and height of logo container
+
   svgString =
   '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
 // <g> tag wraps <text> tag so that user font input layers on top of polygon -> not behind
 svgString += "<g>";
-// Takes user input for shape choice and inserts it into SVG file
+
 svgString += `${answers.shape}`;
+
+  // Conditional check takes users input from choices array and then adds polygon properties and shape color to SVG string
+  let shapeChoice;
+  if (answers.shape === "Triangle") {
+    shapeChoice = new Triangle();
+    svgString += `<polygon points="150, 18 244, 182 56, 182" fill="${answers.shapeBackgroundColor}"/>`;
+  } else if (answers.shape === "Square") {
+    shapeChoice = new Square();
